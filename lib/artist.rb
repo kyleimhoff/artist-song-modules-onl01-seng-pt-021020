1ec3f482d685f336095533d345fea8bec2cef0e2
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-15 19:56:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,9 +33,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
