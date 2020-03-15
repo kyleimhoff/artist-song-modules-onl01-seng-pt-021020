8ed122d774a552ac7712c6e8dabab62971c97c93
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-15 19:56:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,20 +18,8 @@ class Song
     @@songs
   end
 
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-  #end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-  extend Memorable
 end
