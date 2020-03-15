ed51f1f911c2de90405d324538211f52aabe646d
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-15 19:58:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,9 +10,9 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    self.class.all << self
     @songs = []
-  end
+end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
